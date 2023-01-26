77d9774beb1921f837570248e7ed1ff7432a2b28
Panzazeta <lucasbenielli@gmail.br>
2023-01-26 09:12:06 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+//Import styles
+import "./styles/app.scss";
+//Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+//import data
+import data from "./util"
+
+function App() {
+  //state
+  const [songs, setSongs] = useState (data());
+  const [currentSong, setCurrentSong] = useState (songs[0]);
+  const [isPlaying, setIsPlaying] = useState (false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
+    </div>
+  );
+}
+
+export default App;
