c3bfeab469c6159a32a567136b7d645ab9d50420
Panzazeta <lucasbenielli@gmail.br>
2023-02-01 07:49:30 -0300

    change var

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import "./styles/app.scss";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 //import data
 import data from "./util"
 
 function App() {
   //state
   const [songs, setSongs] = useState (data());
-  const [currentSong, setCurrentSong] = useState (songs[2]);
+  const [currentSong, setCurrentSong] = useState (songs[0]);
   const [isPlaying, setIsPlaying] = useState (false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
+      <Player 
+        setIsPlaying={setIsPlaying} 
+        isPlaying={isPlaying} 
+        currentSong={currentSong} 
+      />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
     </div>
   );
 }
