1293232ddc82a73f93f81e2f37f922e855336f0d
Panzazeta <lucasbenielli@gmail.br>
2023-02-06 11:33:54 -0300

    set await

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,5 @@
 import React,{useEffect} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { playAudio } from "../util";
 import { 
     faPlay, 
     faAngleLeft, 
@@ -54,20 +53,20 @@ const Player = ({
             audioRef.current.currentTime = e.target.value;
             setSongInfo({...songInfo, currentTime: e.target.value});
         }
-        const skipTrackHandler= (direction) => {
+        const skipTrackHandler= async (direction) => {
             let currentIndex= songs.findIndex((song) => song.id===currentSong.id);
             if (direction === "skip-forward") {
-                setCurrentSong(songs[(currentIndex + 1) % songs.length])
+            await setCurrentSong(songs[(currentIndex + 1) % songs.length])
             }
             if (direction === "skip-back") {
                 if((currentIndex - 1) % songs.length === - 1){
-                    setCurrentSong(songs[songs.length -1]);
-                    playAudio(isPlaying,audioRef);
+                    await setCurrentSong(songs[songs.length -1]);
+                    if (isPlaying) audioRef.current.play();
                     return
                 }
-                setCurrentSong(songs[(currentIndex - 1) % songs.length])
+                await setCurrentSong(songs[(currentIndex - 1) % songs.length])
             }
-            playAudio(isPlaying,audioRef);
+            if (isPlaying) audioRef.current.play();
         }
     return (
         <div className="player">
